914fc2b93c993b470fdb018ea48cb062ab003937
Sawyer Lightsey <sawyerlightsey@gmail.com>
2022-05-02 21:23:31 -0400

    continued test attempt

--- tests/q1.test.js
+++ tests/q1.test.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { render, fireEvent } from "./test-utils";
+import { render, fireEvent, screen } from "./test-utils";
 import Question1 from "../pages/q1";
 import { act } from "react-dom/test-utils";
 
 
-
-
 describe('Question1', () => {
+
   const q1Instance = render(<Question1 />)
 
-  const input = q1Instance.getByTestId('input')
-  const result = q1Instance.getByTestId('result_val')
-  const button = q1Instance.getByTestId("submit")
 
-  const mockCallBack = jest.fn();
+
+  const input = screen.getByRole('textbox')
+  const result = screen.getByRole('heading', { level: 2 })
+  const button = screen.getByRole("button")
+
   test('Should render blank', () => {
     expect(input.value).toBe("")
   })
 
   test('Simulate user input', () => {
+    const setState = jest.fn();
+    const useStateMock = (useState) => [useState, setState]
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
     fireEvent.change(input, { target: { value: 'test' } })
-    expect(input.value).toBe("test")
+    expect(setState).toHaveBeenCalled();
   })
-
-  
 })
